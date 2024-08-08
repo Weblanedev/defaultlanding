278f09bba1a1d0d6b28be593eeebd60683b8f8b8
Olu-martins <mayowasamuel@gmail.com>
2024-08-08 08:34:52 +0100

    welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
   return (
     <div className="coming-soon-wrapper">
       <div className="coming-soon-content">
-        <h1>Welcome to Our Website</h1>
-        <h3>We're preparing something amazing. Stay tuned!</h3>
-        <div className="coming-soon-image">
-          <img src="path-to-your-image.jpg" alt="Coming Soon" />
-        </div>
-        <h5>Thank you for your patience. We look forward to sharing our up to date offers with you soon.</h5>
+        <h1>Welcome</h1>
+        <h2>We're preparing something amazing. Stay tuned!</h2>
+        {/* <div className="coming-soon-image">
+          <img height={400} width={600} src="src/assets/workdesk.jpg" alt="Coming Soon" />
+        </div> */}
+        <h4>Thank you for your patience. We look forward to sharing our up to date offers with you soon.</h4>
       </div>
     </div>
   );
