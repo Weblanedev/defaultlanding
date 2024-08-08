dd9b308cb2e5f874fcedc89d99ce783910a24219
Olu-martins <mayowasamuel@gmail.com>
2024-08-08 08:19:34 +0100

    chore: set up

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="coming-soon-wrapper">
+      <div className="coming-soon-content">
+        <h1>Welcome to Our Website</h1>
+        <h3>We're preparing something amazing. Stay tuned!</h3>
+        <div className="coming-soon-image">
+          <img src="path-to-your-image.jpg" alt="Coming Soon" />
+        </div>
+        <h5>Thank you for your patience. We look forward to sharing our up to date offers with you soon.</h5>
+      </div>
+    </div>
+  );
+}
+
+export default App
